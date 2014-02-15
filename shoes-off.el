daimrod/shoes-off

commit 930ec700dba0b99faa48d96e165242baf47ce291

shoes-off.el (shoes-off/handle-request): Makes shoes-off's RCIRC instances aware of messages sent from the shoes-off's clients when it makes sense

shoes-off.el
@@ -405,10 +405,10 @@ is not sent to the IRC session.")
   "Handle the request."
   (string-match "\\([A-Z]+\\) \\([^ ]+\\)\\( :\\(.*\\)\\)*" request)
   (destructuring-bind
-        (command args text) (list
-                             (match-string-no-properties 1 request)
-                             (match-string-no-properties 2 request)
-                             (match-string-no-properties 4 request))
+      (command args text) (list
+                           (match-string-no-properties 1 request)
+                           (match-string-no-properties 2 request)
+                           (match-string-no-properties 4 request))
     (let ((username (plist-get authenticated :username))
           (command-sym (intern command)))
       (case command-sym
@@ -425,7 +425,13 @@ is not sent to the IRC session.")
                   username args text))
                (funcall
                 shoes-off-privmsg-plugin username args text process))
-             (rcirc-send-string session request))))))))
+             (let ((channel-name (concat args "@" (process-name session))))
+               (aif (get-buffer channel-name)
+                   (with-current-buffer it
+                     (goto-char (point-max))
+                     (insert text)
+                     (rcirc-send-input))
+                 (rcirc-send-string session request))))))))))
 
 (defun shoes-off/filter (process data)
   "Stuff from the bouncer's client."

commit 83007b7a4702e0ed1756f28369adb7d9666fba3e

* shoes-off.el (shoes-off/handle-request): Fix a bug introduced in 930ec70
Synchronize channels between the remote client and shoes-off client only
when for PRIVMSG. For other commands, it is done "automatically".

e.g. TOPIC command:
<remote> /topic new topic
Both the remote and shoes-off RCIRC clients see
*** remote TOPIC this is a new topic

shoes-off.el
@@ -410,28 +410,27 @@ is not sent to the IRC session.")
                            (match-string-no-properties 2 request)
                            (match-string-no-properties 4 request))
     (let ((username (plist-get authenticated :username))
-          (command-sym (intern command)))
+          (command-sym (intern command))
+          (session (shoes-off/get-session process)))
       (case command-sym
         ('QUIT
          (message "shoes-off bouncer disconnect %s" username))
+        ('PRIVMSG
+         (catch :shoes-off-escape-privmsg
+           (when (functionp shoes-off-privmsg-plugin)
+             (when shoes-off/handle-request-privmsg-logging
+               (message
+                "shoes-off/handle-request %s %S /%s/"
+                username args text))
+             (funcall
+              shoes-off-privmsg-plugin username args text process))
+           (with-current-buffer (or (rcirc-get-buffer session args)
+                                    (rcirc-get-buffer-create session args))
+             (goto-char (point-max))
+             (insert text)
+             (rcirc-send-input)))))
         (t
-         (let ((session (shoes-off/get-session process)))
-           (catch :shoes-off-escape-privmsg
-             (when (and (eq command-sym 'PRIVMSG)
-                        (functionp shoes-off-privmsg-plugin))
-               (when shoes-off/handle-request-privmsg-logging
-                 (message
-                  "shoes-off/handle-request %s %S /%s/"
-                  username args text))
-               (funcall
-                shoes-off-privmsg-plugin username args text process))
-             (let ((channel-name (concat args "@" (process-name session))))
-               (aif (get-buffer channel-name)
-                   (with-current-buffer it
-                     (goto-char (point-max))
-                     (insert text)
-                     (rcirc-send-input))
-                 (rcirc-send-string session request))))))))))
+         (rcirc-send-string session request))))))
 
 (defun shoes-off/filter (process data)
   "Stuff from the bouncer's client."